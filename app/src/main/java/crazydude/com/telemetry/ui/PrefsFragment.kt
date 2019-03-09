a3319c2ed9180e526b930f7d3afaa45d097c91f0
CrazyDude1994 <crazyserega1994@mail.ru>
2019-03-09 22:51:23 +0200

    Converted java to kotlin

---
+++ app/src/main/java/crazydude/com/telemetry/ui/PrefsFragment.kt
@@ -0,0 +1,13 @@
+package crazydude.com.telemetry.ui
+
+import android.os.Bundle
+import android.support.v7.preference.PreferenceFragmentCompat
+import crazydude.com.telemetry.R
+
+class PrefsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle, rootKey: String) {
+        preferenceManager.sharedPreferencesName = "settings"
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+}
