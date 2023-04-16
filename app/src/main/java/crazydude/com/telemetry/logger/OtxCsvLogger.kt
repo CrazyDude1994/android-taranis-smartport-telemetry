760dd6a6bfbd9f740ef13ca6a7003a48fc2ed919
Mariano Dupont <marianomd@gmail.com>
2023-04-16 18:11:26 -0300

    OpenTX log generation (#40)
    * OpenTX log generation
    
    * Update CrsfDataDecoder.kt
    
    * OpenTX log generation
    
    * Cleanup

---
+++ app/src/main/java/crazydude/com/telemetry/logger/OtxCsvLogger.kt
@@ -0,0 +1,336 @@
+package crazydude.com.telemetry.logger
+
+import android.os.Environment
+import crazydude.com.telemetry.maps.Position
+import crazydude.com.telemetry.protocol.decoder.DataDecoder
+import java.io.File
+import java.io.FileWriter
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class OtxCsvLogger : DataDecoder.Listener {
+
+    private val fileWriter: FileWriter
+    private val file: File?
+    private val timer = Timer()
+
+    private val header = listOf(
+        "Date",
+        "Time",
+        "RSSI(dB)",
+        "1RSS(dB)",
+        "2RSS(dB)",
+        "RQly(%)",
+        "RSNR(dB)",
+        "ANT",
+        "RFMD",
+        "TPWR(mW)",
+        "TRSS(dB)", // Downlink - signal strength
+        "TQly(%)",
+        "TSNR(dB)",
+        "Ptch(rad)",
+        "Roll(rad)",
+        //"Yaw(rad)",
+        "FM",
+        "VSpd(m/s)",
+        "GPS",
+        "GSpd(kmh)",
+        "Hdg(°)",
+        "Alt(m)",
+        "Sats",
+//            "RxBt(V)",
+        "Curr(A)",
+        "VFAS(V)",
+        "Dist(m)"
+    )
+
+    init {
+        val name = SimpleDateFormat("yyyy-MM-dd HH-mm-ss").format(Date())
+        val dir = Environment.getExternalStoragePublicDirectory("TelemetryLogs")
+        dir.mkdirs()
+        file = File(dir, "$name.csv")
+        fileWriter = FileWriter(file)
+        outputLine(header)
+        timer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                outputData()
+            }
+        },1000,200)
+    }
+
+    private var fuel: Int = 0
+    private var latitude: Double = 0.0
+    private var longitude: Double = 0.0
+    private var batVoltage: Float = 0F
+    private var cellVoltage: Float = 0F
+    private var current: Float = 0F
+    private var heading: Float = 0F
+    private var rssi: Int = 0
+    private var upLq: Int = 0
+    private var dnLq: Int = 0
+    private var elrsMode: Int = 0
+    private var satellites: Int = 0
+    private var gpsFix: Boolean = false
+    private var vSpeed: Float = 0F
+    private var throttle: Int = 0;
+    private var altitude: Float = 0F
+    private var gpsAltitude: Float = 0F
+    private var distance: Int = 0
+    private var rollAngle: Float = 0F
+    private var pitchAngle: Float = 0F
+    private var gSpeed: Float = 0F
+    private var armed: Boolean = false
+    private var airSpeed: Float = 0F
+    private var rcChannels: IntArray? = null
+    private var statusText: String = ""
+    private var dnSnr: Int = 0
+    private var upSnr: Int = 0
+    private var activeAntenna: Int = 0
+    private var power: Int = 0
+    private var rssiDbm1: Int = 0
+    private var rssiDbm2: Int = 0
+    private var rssiDbmd: Int = 0
+    private var flightMode: String = ""
+
+    private fun outputLine(line: List<String>) {
+        val csv = line.joinToString(",")
+        fileWriter.append(csv)
+        fileWriter.append("\n")
+    }
+
+    /*
+    *
+    * "Alt(m)" altitude 0.0
+      -        armed false
+      -        hdg false
+      "FM" firstFlightMode ERROR
+      "Curr(A)"  current: 0.4
+      "TSNR(dB)"  dn snr 4
+      "TQly(%)"   dnLq: 100
+      "RFMD"      elrs mode: 5
+      "Fuel(mAh)" fuel: 0 mah
+      "GPS"       GPS: 0.0 0.0
+      "GSpd(kmh)" ground speed 0.0
+      "Hdg(°)"    heading: 103.89444
+      "Ptch(rad)" pitchAngle -81.49752
+      "TPWR(mW)"  power 1
+      "Roll(rad)" rollAngle 83.99561
+      "1RSS(dB)"  rssi dbm1 -40
+      "2RSS(dB)"  rssi dbm2 0
+      "TRSS(dB)"  rssi dbmd -16
+      "RSSI(dB)"  rssi: 94
+      "Sats"      sats 0
+      -           fix: false
+      "RSNR(dB)"  up snr 2
+      "RQly(%)"   upLq: 100
+      "VFAS(V)"   voltage vbat or cell 16.4
+
+    * */
+    private fun outputData() {
+        val date = SimpleDateFormat("yyyy-MM-dd").format(Date())
+        val time = SimpleDateFormat("HH:mm:ss.SSS").format(Date())
+
+        val data = listOf<String>(
+            date,
+            time,
+            rssi.toString(),
+            rssiDbm1.toString(),
+            rssiDbm2.toString(),
+            upLq.toString(),
+            upSnr.toString(),
+            activeAntenna.toString(),
+            elrsMode.toString(),
+            power.toString(),
+            rssiDbmd.toString(), // Downlink - signal strength
+            dnLq.toString(),
+            dnSnr.toString(),
+            pitchAngle.toString(),
+            rollAngle.toString(),
+            flightMode,
+            vSpeed.toString(),
+            "$latitude $longitude",
+            gSpeed.toString(),
+            heading.toString(),
+            altitude.toString(),
+            satellites.toString(),
+//            "RxBt(V)",
+            current.toString(),
+            batVoltage.toString(),
+            distance.toString()
+        )
+        outputLine(data)
+    }
+
+    override fun onConnectionFailed() {
+
+    }
+
+    override fun onFuelData(fuel: Int) {
+        this.fuel = fuel
+
+    }
+
+    override fun onConnected() {
+
+    }
+
+    override fun onGPSData(latitude: Double, longitude: Double) {
+        this.latitude = latitude
+        this.longitude = longitude
+    }
+
+    override fun onGPSData(list: List<Position>, addToEnd: Boolean) {
+
+    }
+
+    override fun onVBATData(voltage: Float) {
+        this.batVoltage = voltage
+    }
+
+    override fun onCellVoltageData(voltage: Float) {
+        this.cellVoltage = voltage
+    }
+
+    override fun onCurrentData(current: Float) {
+        this.current = current
+    }
+
+    override fun onHeadingData(heading: Float) {
+        this.heading = heading
+    }
+
+    override fun onRSSIData(rssi: Int) {
+        this.rssi = rssi
+    }
+
+    override fun onUpLqData(lq: Int) {
+        this.upLq = lq
+    }
+
+    override fun onDnLqData(lq: Int) {
+        this.dnLq = lq
+    }
+
+    override fun onElrsModeModeData(mode: Int) {
+        this.elrsMode = mode
+    }
+
+    override fun onDisconnected() {
+//        val sendIntent = Intent()
+//        sendIntent.action = Intent.ACTION_SEND
+//        sendIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file))
+//        sendIntent.type = "text/csv"
+//        startActivity(Intent.createChooser(sendIntent, "SHARE"))
+        timer.cancel()
+        timer.purge()
+        fileWriter.close()
+    }
+
+    override fun onGPSState(satellites: Int, gpsFix: Boolean) {
+        this.satellites = satellites
+        this.gpsFix = gpsFix
+    }
+
+    override fun onVSpeedData(vspeed: Float) {
+        this.vSpeed = vspeed
+    }
+
+    override fun onThrottleData(throttle: Int) {
+        this.throttle = throttle
+    }
+
+    override fun onAltitudeData(altitude: Float) {
+        this.altitude = altitude
+    }
+
+    override fun onGPSAltitudeData(altitude: Float) {
+        this.gpsAltitude = altitude
+    }
+
+    override fun onDistanceData(distance: Int) {
+        this.distance = distance
+    }
+
+    override fun onRollData(rollAngle: Float) {
+        this.rollAngle = rollAngle
+    }
+
+    override fun onPitchData(pitchAngle: Float) {
+        this.pitchAngle = pitchAngle
+    }
+
+    override fun onGSpeedData(speed: Float) {
+        this.gSpeed = speed
+    }
+
+    override fun onFlyModeData(
+        armed: Boolean,
+        heading: Boolean,
+        firstFlightMode: DataDecoder.Companion.FlyMode?,
+        secondFlightMode: DataDecoder.Companion.FlyMode?
+    ) {
+        this.flightMode = firstFlightMode.toString()
+    }
+
+    override fun onAirSpeedData(speed: Float) {
+        this.airSpeed=speed
+    }
+
+    override fun onRCChannels(rcChannels: IntArray) {
+        // not yet implemented
+    }
+
+    override fun onStatusText(message: String) {
+        this.statusText=message
+    }
+
+    override fun onDNSNRData(snr: Int) {
+        this.dnSnr=snr
+    }
+
+    override fun onUPSNRData(snr: Int) {
+        this.upSnr=snr
+    }
+
+    override fun onAntData(activeAntena: Int) {
+        this.activeAntenna=activeAntena
+    }
+
+    override fun onPowerData(power: Int) {
+        this.power=power
+    }
+
+    /**
+     * Uplink - received signal strength antenna 1 (RSSI)
+     */
+    override fun onRssiDbm1Data(rssi: Int) {
+        this.rssiDbm1=rssi
+    }
+
+    /**
+     * Uplink - received signal strength antenna 2 (RSSI)
+     */
+    override fun onRssiDbm2Data(rssi: Int) {
+        this.rssiDbm2=rssi
+    }
+
+    /**
+     * Downlink - received signal strength (RSSI)
+     */
+    override fun onRssiDbmdData(rssi: Int) {
+        this.rssiDbmd=rssi
+    }
+
+    override fun onVBATOrCellData(voltage: Float) {
+        this.batVoltage=voltage
+    }
+
+    override fun onTelemetryByte() {
+
+    }
+
+    override fun onSuccessDecode() {
+
+    }
+}
--- app/src/main/java/crazydude/com/telemetry/service/DataService.kt
+++ app/src/main/java/crazydude/com/telemetry/service/DataService.kt
@@ -1,5 +1,6 @@
 package crazydude.com.telemetry.service
 
+import android.Manifest
 import android.annotation.TargetApi
 import android.app.*
 import android.bluetooth.BluetoothDevice
@@ -14,6 +15,7 @@ import androidx.core.content.ContextCompat
 import com.hoho.android.usbserial.driver.UsbSerialPort
 import crazydude.com.telemetry.R
 import crazydude.com.telemetry.api.*
+import crazydude.com.telemetry.logger.OtxCsvLogger
 import crazydude.com.telemetry.manager.PreferenceManager
 import crazydude.com.telemetry.maps.Position
 import crazydude.com.telemetry.protocol.pollers.BluetoothDataPoller
@@ -37,6 +39,7 @@ class DataService : Service(), DataDecoder.Listener {
 
     private var dataPoller: DataPoller? = null
     private var dataListener: DataDecoder.Listener? = null
+    private var logListener: OtxCsvLogger? = null
     private val dataBinder = DataBinder()
     private var hasGPSFix = false
     private var isArmed = false
@@ -62,14 +65,21 @@ class DataService : Service(), DataDecoder.Listener {
                 NotificationChannel("bt_channel", "Bluetooth", importance)
             val notificationManager: NotificationManager =
                 getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-                notificationManager.createNotificationChannel(channel)
+            notificationManager.createNotificationChannel(channel)
         }
 
 
         notification = NotificationCompat.Builder(this, "bt_channel")
             .setContentText("Telemetry service is running. To stop - disconnect and close the app")
             .setContentTitle("Telemetry service is running")
-            .setContentIntent(PendingIntent.getActivity(this, -1, Intent(this, MapsActivity::class.java), 0))
+            .setContentIntent(
+                PendingIntent.getActivity(
+                    this,
+                    -1,
+                    Intent(this, MapsActivity::class.java),
+                    0
+                )
+            )
             .setSmallIcon(R.drawable.ic_launcher_foreground)
             .build()
     }
@@ -85,11 +95,13 @@ class DataService : Service(), DataDecoder.Listener {
     }
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
-    fun connect(device: BluetoothDevice, isBle : Boolean) {
+    fun connect(device: BluetoothDevice, isBle: Boolean) {
         try {
             dataPoller?.disconnect()
 
             val logFile = createLogFile()
+
+            createLogger()
 
             if (!isBle) {
                 val socket =
@@ -114,7 +126,7 @@ class DataService : Service(), DataDecoder.Listener {
         }
     }
 
-    private fun createLogFile() : FileOutputStream? {
+    private fun createLogFile(): FileOutputStream? {
         var fileOutputStream: FileOutputStream? = null
         if (preferenceManager.isLoggingEnabled()
             && ContextCompat.checkSelfPermission(
@@ -134,6 +146,7 @@ class DataService : Service(), DataDecoder.Listener {
 
     fun connect(serialPort: UsbSerialPort, connection: UsbDeviceConnection) {
         val logFile = createLogFile()
+        createLogger()
         dataPoller = UsbDataPoller(
             this,
             serialPort,
@@ -141,6 +154,17 @@ class DataService : Service(), DataDecoder.Listener {
             connection,
             logFile
         )
+    }
+
+    private fun createLogger() {
+        if (preferenceManager.isLoggingEnabled()
+            && ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            logListener = OtxCsvLogger()
+        }
     }
 
     fun setDataListener(dataListener: DataDecoder.Listener?) {
@@ -170,15 +194,18 @@ class DataService : Service(), DataDecoder.Listener {
 
     override fun onConnectionFailed() {
         dataListener?.onConnectionFailed()
+        logListener?.onConnectionFailed()
         dataPoller = null
     }
 
     override fun onFuelData(fuel: Int) {
         dataListener?.onFuelData(fuel)
+        logListener?.onFuelData(fuel)
     }
 
     override fun onConnected() {
         dataListener?.onConnected()
+        logListener?.onConnected()
 
         if (preferenceManager.isSendDataEnabled()) {
             createSession()
@@ -221,13 +248,23 @@ class DataService : Service(), DataDecoder.Listener {
         apiHandler.postDelayed({
             if (hasGPSFix && isArmed) {
                 ApiManager.apiService.sendData(
-                    AddLogRequest(sessionId, lastLatitude, lastLongitude, lastAltitude, lastHeading, lastSpeed)
+                    AddLogRequest(
+                        sessionId,
+                        lastLatitude,
+                        lastLongitude,
+                        lastAltitude,
+                        lastHeading,
+                        lastSpeed
+                    )
                 ).enqueue(object : Callback<AddLogResponse?> {
                     override fun onFailure(call: Call<AddLogResponse?>, t: Throwable) {
                         sendTelemetryData(sessionId)
                     }
 
-                    override fun onResponse(call: Call<AddLogResponse?>, response: Response<AddLogResponse?>) {
+                    override fun onResponse(
+                        call: Call<AddLogResponse?>,
+                        response: Response<AddLogResponse?>
+                    ) {
                         try {
                             sendTelemetryData(sessionId)
                         } catch (e: NullPointerException) {
@@ -248,6 +285,7 @@ class DataService : Service(), DataDecoder.Listener {
         lastLatitude = latitude
         lastLongitude = longitude
         dataListener?.onGPSData(latitude, longitude)
+        logListener?.onGPSData(latitude, longitude)
     }
 
     override fun onGPSData(list: List<Position>, addToEnd: Boolean) {
@@ -256,56 +294,69 @@ class DataService : Service(), DataDecoder.Listener {
 
     override fun onVBATData(voltage: Float) {
         dataListener?.onVBATData(voltage)
+        logListener?.onVBATData(voltage)
     }
 
     override fun onCellVoltageData(voltage: Float) {
         dataListener?.onCellVoltageData(voltage)
+        logListener?.onCellVoltageData(voltage)
     }
 
     override fun onVBATOrCellData(voltage: Float) {
         dataListener?.onVBATOrCellData(voltage)
+        logListener?.onVBATOrCellData(voltage)
     }
 
     override fun onCurrentData(current: Float) {
         dataListener?.onCurrentData(current)
+        logListener?.onCurrentData(current)
     }
 
     override fun onHeadingData(heading: Float) {
         lastHeading = heading
         dataListener?.onHeadingData(heading)
+        logListener?.onHeadingData(heading)
     }
 
     override fun onAirSpeedData(speed: Float) {
         dataListener?.onAirSpeedData(speed)
+        logListener?.onAirSpeedData(speed)
     }
 
     override fun onTelemetryByte() {
         dataListener?.onTelemetryByte()
+        logListener?.onTelemetryByte()
     }
 
     override fun onSuccessDecode() {
         dataListener?.onSuccessDecode()
+        logListener?.onSuccessDecode()
     }
 
     override fun onRSSIData(rssi: Int) {
         dataListener?.onRSSIData(rssi)
+        logListener?.onRSSIData(rssi)
     }
 
     override fun onUpLqData(lq: Int) {
         dataListener?.onUpLqData(lq)
+        logListener?.onUpLqData(lq)
     }
 
     override fun onDnLqData(lq: Int) {
         dataListener?.onDnLqData(lq)
+        logListener?.onDnLqData(lq)
     }
 
     override fun onElrsModeModeData(mode: Int) {
         dataListener?.onElrsModeModeData(mode)
+        logListener?.onElrsModeModeData(mode)
     }
 
     override fun onDisconnected() {
         points.clear()
         dataListener?.onDisconnected()
+        logListener?.onDisconnected()
         dataPoller = null
         satellites = 0
         hasGPSFix = false
@@ -315,40 +366,49 @@ class DataService : Service(), DataDecoder.Listener {
     override fun onGPSState(satellites: Int, gpsFix: Boolean) {
         hasGPSFix = gpsFix
         dataListener?.onGPSState(satellites, gpsFix)
+        logListener?.onGPSState(satellites, gpsFix)
     }
 
     override fun onVSpeedData(vspeed: Float) {
         dataListener?.onVSpeedData(vspeed)
+        logListener?.onVSpeedData(vspeed)
     }
 
-    override fun onThrottleData(throttle :Int) {
+    override fun onThrottleData(throttle: Int) {
         dataListener?.onThrottleData(throttle)
+        logListener?.onThrottleData(throttle)
     }
 
     override fun onAltitudeData(altitude: Float) {
         lastAltitude = altitude
         dataListener?.onAltitudeData(altitude)
+        logListener?.onAltitudeData(altitude)
     }
 
     override fun onGPSAltitudeData(altitude: Float) {
         dataListener?.onGPSAltitudeData(altitude)
+        logListener?.onGPSAltitudeData(altitude)
     }
 
     override fun onDistanceData(distance: Int) {
         dataListener?.onDistanceData(distance)
+        logListener?.onDistanceData(distance)
     }
 
     override fun onRollData(rollAngle: Float) {
         dataListener?.onRollData(rollAngle)
+        logListener?.onRollData(rollAngle)
     }
 
     override fun onPitchData(pitchAngle: Float) {
         dataListener?.onPitchData(pitchAngle)
+        logListener?.onPitchData(pitchAngle)
     }
 
     override fun onGSpeedData(speed: Float) {
         lastSpeed = speed
         dataListener?.onGSpeedData(speed)
+        logListener?.onGSpeedData(speed)
     }
 
     override fun onFlyModeData(
@@ -359,42 +419,52 @@ class DataService : Service(), DataDecoder.Listener {
     ) {
         isArmed = armed
         dataListener?.onFlyModeData(armed, heading, firstFlightMode, secondFlightMode)
+        logListener?.onFlyModeData(armed, heading, firstFlightMode, secondFlightMode)
     }
 
     override fun onRCChannels(rcChannels: IntArray) {
         dataListener?.onRCChannels(rcChannels)
+        logListener?.onRCChannels(rcChannels)
     }
 
     override fun onStatusText(message: String) {
         dataListener?.onStatusText(message)
+        logListener?.onStatusText(message)
     }
 
     override fun onDNSNRData(snr: Int) {
         dataListener?.onDNSNRData(snr)
+        logListener?.onDNSNRData(snr)
     }
 
     override fun onUPSNRData(snr: Int) {
         dataListener?.onUPSNRData(snr)
+        logListener?.onUPSNRData(snr)
     }
 
     override fun onAntData(activeAntena: Int) {
         dataListener?.onAntData(activeAntena)
+        logListener?.onAntData(activeAntena)
     }
 
     override fun onPowerData(power: Int) {
         dataListener?.onPowerData(power)
+        logListener?.onPowerData(power)
     }
 
     override fun onRssiDbm1Data(rssi: Int) {
         dataListener?.onRssiDbm1Data(rssi)
+        logListener?.onRssiDbm1Data(rssi)
     }
 
     override fun onRssiDbm2Data(rssi: Int) {
         dataListener?.onRssiDbm2Data(rssi)
+        logListener?.onRssiDbm2Data(rssi)
     }
 
     override fun onRssiDbmdData(rssi: Int) {
         dataListener?.onRssiDbmdData(rssi)
+        logListener?.onRssiDbmdData(rssi)
     }
 
     fun disconnect() {
